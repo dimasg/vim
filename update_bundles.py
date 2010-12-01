b0a80fda1ee1567518c20fa7c8d36c3f12a15617
Dmitriy Grachjev <dvg@post.ru>
2010-12-01 17:28:39 +0300

    jslint added

--- update_bundles.py
+++ update_bundles.py
@@ -21,7 +21,8 @@ def remove_readonly(fn, path, excinfo):
 
 pathogen_git = "git://github.com/tpope/vim-pathogen.git";
 
-git_bundles = [ 
+git_bundles = [
+    "git://github.com/joestelmach/javaScriptLint.vim.git",
     "git://github.com/motemen/git-vim.git",
 #    "git://github.com/scrooloose/nerdtree.git",
     "git://github.com/kevinw/pyflakes-vim.git",
