39c935eab50b3727c3fb70456c0f54cbb8b9fc9a
Dmitriy Grachjev <dvg@post.ru>
2016-06-14 02:16:44 +0300

    removed BOM for js, php and python settings

--- .vimrc.js
+++ .vimrc.js
@@ -1,2 +1,2 @@
-﻿let g:syntastic_javascript_checkers=['jshint', 'jsl', 'jslint']
+let g:syntastic_javascript_checkers=['jshint', 'jsl', 'jslint']
 let g:syntastic_html_checkers=['jshint', 'jsl', 'jslint']
