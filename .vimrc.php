ca55aa75cef030e3fdc3f91aec49254cf57c70bc
Dmitriy Grachjev <dvg@post.ru>
2016-06-14 02:16:44 +0300

    removed BOM for js, php and python settings

--- .vimrc.php
+++ .vimrc.php
@@ -1,4 +1,4 @@
-﻿let g:syntastic_php_checkers=['php', 'phpcs']
+let g:syntastic_php_checkers=['php', 'phpcs']
 let g:syntastic_php_php_exec = 'php'
 let g:syntastic_php_php_args = '-l'
 let g:syntastic_php_phpcs_args='--tab-width=0 --standard=php_style.xml'
